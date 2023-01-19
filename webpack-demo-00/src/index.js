ea186b73a535149734ce3314d32772f61d76e4c2
aliyu <1324149139@qq.com>
2023-01-19 16:18:50 +0800

    显示你好世界

---
+++ webpack-demo-00/src/index.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+const a = 'Hello World110';
+console.log(a);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 2));
+
+const x = document.getElementById('x');
+x.innerText = 'Hello World';
---
+++ webpack-demo-00/webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'index.html',
+        title: "Webpack App"
+    })],
+};
