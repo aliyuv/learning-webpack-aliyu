38f5dc56df29c23413d281f43493a2d0dab0302e
aliyu <1324149139@qq.com>
2023-01-19 16:23:46 +0800

    体验了工程化开发的流程

--- webpack-demo-00/src/index.js
+++ webpack-demo-00/src/index.js
@@ -5,6 +5,22 @@ function add(a, b) {
     return a + b;
 }
 console.log(add(1, 2));
+//
+// const x = document.getElementById('x');
+// x.innerText = 'Hello World';
 
-const x = document.getElementById('x');
-x.innerText = 'Hello World';
+// in webpack use axios request data
+import axios from 'axios'
+axios.get('/api/user').then((res) => {
+    console.log(res);
+}, (err) => {
+    console.log(err);
+});
+
+//in webpack use vue
+import {createApp} from 'vue'
+console.log(createApp);
+
+//in webpack use react
+import React from 'react'
+console.log(React);
